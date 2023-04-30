fb853ee1cfb263a285bbfd4f76938668defaec97
erik-nathan <eriknathan.contato@gmail.com>
2023-04-30 17:51:34 -0300

    feat: adicioanando configurações do banco de dados

--- project/settings.py
+++ project/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'deltareboqueDB',
+        'USER': 'deltareboque',
+        'PASSWORD': 'deltareboquepass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
