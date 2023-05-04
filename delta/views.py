ca36e346b859aea133e3ee713da137ba45ab4627
erik-nathan <eriknathan.contato@gmail.com>
2023-05-04 11:32:45 -0300

    feat: adicionando função de paginação e busca

--- delta/views.py
+++ delta/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render, redirect
 from delta.forms import CadastroForm
-from django.http import HttpResponse
-from django.template.loader import get_template
-from weasyprint import HTML
 from delta.models import Cadastro
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def home(request):
     data = {}
-    data['db'] = Cadastro.objects.all()
+    search = request.GET.get('search')
+    all_items = Cadastro.objects.all().order_by('-id')
+
+    if search:
+        search_results = all_items.filter(sinistro__icontains=search)
+        paginator = Paginator(search_results, 10)
+    else:
+        paginator = Paginator(all_items, 10)
+
+    page = request.GET.get('page')
+    try:
+        db = paginator.page(page)
+    except PageNotAnInteger:
+        db = paginator.page(1)
+    except EmptyPage:
+        db = paginator.page(paginator.num_pages)
+
+    data['db'] = db
     return render(request, 'index.html', data)
 
 
